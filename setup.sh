0f5bd2fcee0ff2e6d0d64fb4b2d93815266beda8
Entropy Xu <entropy.xcy@hotmail.com>
2018-10-16 16:49:02 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@ sudo apt-get update
 sudo apt-get install google-drive-ocamlfuse -y
 mkdir ~/googledrive
 sudo sed -i 's/#user_allow_other/user_allow_other/g' /etc/fuse.conf
-sudo apt-get install nfs-kernel-server samba firefox -y
+sudo apt-get install nfs-kernel-server samba firefox fuse -y
 
 echo 'Setting up smb..'
 sudo smbpasswd -a $USER
